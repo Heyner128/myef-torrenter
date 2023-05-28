7bfc6ac4aca5d6b2f14416aaee5774794126688f
Heyner Cuevas <73818312+Heyner128@users.noreply.github.com>
2023-05-28 03:57:38 +0200

    hotfix

--- src/TorrentBot.ts
+++ src/TorrentBot.ts
@@ -169,6 +169,7 @@ export default class TorrentBot {
       };
     } catch (err) {
       logger.error(err);
+      this.isSearching = false;
       return { message: `Error de busqueda` };
     }
   }

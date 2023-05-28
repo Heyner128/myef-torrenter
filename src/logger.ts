a2ebae97ca30f384e460a3a61fecbb9bcbb667fd
Heyner Cuevas <73818312+Heyner128@users.noreply.github.com>
2023-05-28 15:02:27 +0200

    hotfix

--- src/logger.ts
+++ src/logger.ts
@@ -2,19 +2,25 @@ import winston from "winston";
 
 const logger = winston.createLogger({
   level: "info",
-  format: winston.format.json(),
+  format: winston.format.simple(),
   defaultMeta: { service: "torrent-bot" },
   exitOnError: false,
   transports: [
     new winston.transports.File({
       filename: "./logs/error.log",
       level: "error",
+      handleExceptions: true,
+      handleRejections: true,
     }),
     new winston.transports.File({
       filename: "./logs/combined.log",
+      handleExceptions: true,
+      handleRejections: true,
     }),
     new winston.transports.Console({
       format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
+      handleExceptions: true,
+      handleRejections: true,
     }),
   ],
 });

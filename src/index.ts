c4ff34ceb42028825ce8e05bbfc4864c3e2cf6d3
Heyner Cuevas <73818312+Heyner128@users.noreply.github.com>
2024-08-17 20:57:39 +0200

    update

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import logger from "./logger.js";
 dotenv.config();
 
 const bot = new TorrentBot({
-  max_download_size_kb: 1000000,
+  max_download_size_kb: 2000000,
   download_speed_limit_kbs: 62500,
 });
 
